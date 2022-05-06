a2130df46d89b02a01d7b167665ab68547f383cb
Ahmed <a.tamer87@yahoo.com>
2022-05-06 05:01:36 +0200

    Added Expenses Section

--- src/pages/Expenses/Expenses.tsx
+++ src/pages/Expenses/Expenses.tsx
@@ -6,6 +6,10 @@ import personsThree from "../../assets/png/person3.png";
 import addIcon from "../../assets/png/addIcon.png";
 import optionIcon from "../../assets/png/menuIcon.png";
 import cartIcon from "../../assets/svg/cartIcon.svg";
+import transportIcon from "../../assets/svg/transportIcon.svg";
+import houseIcon from "../../assets/svg/houseIcon.svg";
+import boxes from "../../assets/png/boxes.png";
+import plant from '../../assets/png/plant.png';
 import { BarChart, Bar, ResponsiveContainer, Cell } from "recharts";
 
 const Expenses = () => {
@@ -139,6 +143,85 @@ const Expenses = () => {
     },
   ];
 
+  const todayExpenses = [
+    {
+      id: 1,
+      expense: "Grocery",
+      time: "5:12 pm",
+      location: "Belanja di pascar",
+      price: 326.8,
+      icon: cartIcon,
+      iconBackgroundColor: "#32a7e2",
+    },
+    {
+      id: 2,
+      expense: "Transportation",
+      time: "5:12 pm",
+      location: "Naik bus umum",
+      price: 15.0,
+      icon: transportIcon,
+      iconBackgroundColor: "#B548C6",
+    },
+    {
+      id: 3,
+      expense: "Housing",
+      time: "5:12 pm",
+      location: "Bayar Listrik",
+      price: 185.75,
+      icon: houseIcon,
+      iconBackgroundColor: "#FF8700",
+    },
+  ];
+
+  const previousExpenses = [
+    {
+      id: 1,
+      expense: "Food and Drink",
+      time: "5:12 pm",
+      location: "Makan Steak",
+      price: 156.0,
+      icon: cartIcon,
+      iconBackgroundColor: "#DC3434",
+    },
+    {
+      id: 2,
+      expense: "Entertainment",
+      time: "5:12 pm",
+      location: "Nonton Bioskop",
+      price: 35.2,
+      icon: transportIcon,
+      iconBackgroundColor: "#4BA83D",
+    },
+  ];
+
+  const spendCategories = [
+    {
+      id: 1,
+      category: "Food and Drinks",
+      price: 872.4,
+    },
+    {
+      id: 2,
+      category: "Shopping",
+      price: 1378.2,
+    },
+    {
+      id: 3,
+      category: "Housing",
+      price: 928.5,
+    },
+    {
+      id: 4,
+      category: "Transportation",
+      price: 420.7,
+    },
+    {
+      id: 5,
+      category: "Vehicle",
+      price: 520,
+    },
+  ];
+
   const onMouseOver = (data: any, index: number) => setActiveIndex(index);
 
   return (
@@ -150,9 +233,21 @@ const Expenses = () => {
               <p className={styles.expensesTitle}>Expenses</p>
               <div className={styles.expensesActions}>
                 <div className={styles.personImages}>
-                  <img className={styles.personOne} src={personsOne} alt="person one" />
-                  <img className={styles.personTwo} src={personsTwo} alt="person one" />
-                  <img className={styles.personThree} src={personsThree} alt="person one" />
+                  <img
+                    className={styles.personOne}
+                    src={personsOne}
+                    alt="person one"
+                  />
+                  <img
+                    className={styles.personTwo}
+                    src={personsTwo}
+                    alt="person one"
+                  />
+                  <img
+                    className={styles.personThree}
+                    src={personsThree}
+                    alt="person one"
+                  />
                 </div>
                 <button>
                   <img className={styles.addIcon} src={addIcon} alt="add" />
@@ -162,8 +257,8 @@ const Expenses = () => {
 
             <p className={styles.dateRange}>01 - 25 March, 2020</p>
 
-            <ResponsiveContainer width="100%" minHeight="9vh">
-              <BarChart width={150} height={40} data={data}>
+            <ResponsiveContainer width="100%" height="9%">
+              <BarChart data={data}>
                 <Bar
                   dataKey="uv"
                   fill="rgba(21, 122, 255, 0.2)"
@@ -192,21 +287,107 @@ const Expenses = () => {
             </div>
 
             <ul>
-              <li className={styles.expenseItem}>
-                <div className={styles.expenseItemLeft}>
-                  <div className={styles.expenseItemDiv}>
-                    <img src={cartIcon} alt="cart" />
+              {todayExpenses.map((expense) => (
+                <li className={styles.expenseItem} key={expense.id}>
+                  <div className={styles.expenseItemLeft}>
+                    <div
+                      style={{ backgroundColor: expense.iconBackgroundColor }}
+                      className={styles.expenseItemDiv}
+                    >
+                      <img src={cartIcon} alt={expense.expense} />
+                    </div>
+                    <div className={styles.expenseItemDetails}>
+                      <p className={styles.expenseItemTitle}>
+                        {expense.expense}
+                      </p>
+                      <p className={styles.expenseItemTime}>
+                        {expense.time} • {expense.location}
+                      </p>
+                    </div>
                   </div>
-                  <div className={styles.expenseItemDetails}>
-                    <p className={styles.expenseItemTitle}>Grocery</p>
-                    <p className={styles.expenseItemTime}>
-                      5:12 pm • Belanja di pasar
+                  <p className={styles.expenseItemPrice}>
+                    {expense.price.toFixed(2)}
+                  </p>
+                </li>
+              ))}
+            </ul>
+
+            <div className={styles.expensesOverviewHeader}>
+              <p className={styles.expensesOverviewTitle}>
+                Monday, 23 March 2020
+              </p>
+              <button>
+                <img className={styles.optionIcon} src={optionIcon} alt="opt" />
+              </button>
+            </div>
+
+            <ul>
+              {previousExpenses.map((expense) => (
+                <li className={styles.expenseItem} key={expense.id}>
+                  <div className={styles.expenseItemLeft}>
+                    <div
+                      style={{ backgroundColor: expense.iconBackgroundColor }}
+                      className={styles.expenseItemDiv}
+                    >
+                      <img src={cartIcon} alt={expense.expense} />
+                    </div>
+                    <div className={styles.expenseItemDetails}>
+                      <p className={styles.expenseItemTitle}>
+                        {expense.expense}
+                      </p>
+                      <p className={styles.expenseItemTime}>
+                        {expense.time} • {expense.location}
+                      </p>
+                    </div>
+                  </div>
+                  <p className={styles.expenseItemPrice}>
+                    {expense.price.toFixed(2)}
+                  </p>
+                </li>
+              ))}
+            </ul>
+          </section>
+
+          <section className={styles.moneyOverview}>
+            <p className={styles.moneyOverviewTitle}>Where'd your money go?</p>
+
+            <ul>
+              {spendCategories.map((category) => (
+                <li key={category.id}>
+                  <div className={styles.spendCategory}>
+                    <p className={styles.spendCategoryName}>
+                      {category.category}
+                    </p>
+                    <p className={styles.spendCategoryPrice}>
+                      {category.price.toFixed(2)}
                     </p>
                   </div>
-                </div>
-                <p className={styles.expenseItemPrice}>-326.80</p>
-              </li>
+                  <div className={styles.spendCategoryBar}>
+                    <div
+                      style={{
+                        width: `${
+                          (category.price /
+                          spendCategories.reduce(
+                            (acc, current) => acc + current.price,
+                            0
+                          )) * 100
+                        }%`,
+                      }}
+                      className={styles.spendCategoryColoredBar}
+                    ></div>
+                  </div>
+                </li>
+              ))}
             </ul>
+
+            <div className={styles.saveMoneyDiv}>
+              <img className={styles.boxes} src={boxes} alt="boxes" />
+              <img className={styles.planet} src={plant} alt="planet" />
+              <p className={styles.saveMoneyTitle}>Save more money</p>
+              <p className={styles.saveMoneyInfo}>eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
+              <button className={styles.button} type="button">VIEW TIPS</button>
+            </div>
+
           </section>
         </div>
       </main>
